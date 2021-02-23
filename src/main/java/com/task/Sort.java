0842a1a3b1b5895f227ea7dd882fd12a6ef96f21
Severina-anastasiia <severina.anastasiia@nure.ua>
2021-02-23 16:44:13 +0200

    change buttons color

--- src/main/java/com/task/Sort.java
+++ src/main/java/com/task/Sort.java
@@ -16,16 +16,13 @@ public class Sort {
     private Random random = new Random();
     private int count = 0;
 
-    public Sort(){
+
+    public Sort() {
 
         //create X buttons with random number and list of this numbers
         createListOfNumberButtons();
-        int[] array = new int[buttons.size()];
-        for(int i = 0; i < buttons.size(); i++)
-            array[i] = Integer.parseInt(buttons.get(i).getText());
         int low = 0;
-        int high = buttons.size()-1;
-
+        int high = Main.userNumber - 1;
         //create button for sort
         sort.setBounds(300, 30, 80, 20);
         sort.setBackground(Color.GREEN);
@@ -35,17 +32,14 @@ public class Sort {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 count++; //for each second click
-                if(count%2 == 0){
-                    quickSort(array, low, high);
-                    for(int i =0 ; i< buttons.size(); i++){
-                        buttons.get(i).setText(String.valueOf(array[i]));
-                    }
-                } else{
-                    quickSort(array, low, high);
-                    for(int i =0, j = buttons.size()-1; i< buttons.size(); i++){
-                        buttons.get(i).setText(String.valueOf(array[j]));
-                        j--;
-                    }
+                if (count % 2 == 0) {
+                    for(int i = 0; i < buttons.size(); i++)
+                        buttons.get(i).setBackground(Color.GREEN);
+                    quickSortRe(buttons, low, high);
+                } else {
+                    for(int i = 0; i < buttons.size(); i++)
+                        buttons.get(i).setBackground(Color.GREEN);
+                    quickSort(buttons, low, high);
                 }
             }
         });
@@ -69,28 +63,28 @@ public class Sort {
         frame.setLayout(null);
         frame.add(sort);
         frame.add(reset);
-        for( int i =0 ; i< buttons.size(); i++){
+        for (int i = 0; i < buttons.size(); i++) {
             frame.add(buttons.get(i));
         }
     }
 
     //check list of numbers if there are any less or equal to 30. if not change first
-    private void checkForThirty(){
-        for(int i =0 ; i< buttons.size(); i++){
-            if(Integer.parseInt(buttons.get(i).getText()) <= 30){
+    private void checkForThirty() {
+        for (int i = 0; i < buttons.size(); i++) {
+            if (Integer.parseInt(buttons.get(i).getText()) <= 30) {
                 return;
             }
         }
         buttons.get(0).setText("30");
     }
 
-    private void createListOfNumberButtons(){
-        for(int i = 0, y = 30, x = 40; i < Main.userNumber; i++){
+    private void createListOfNumberButtons() {
+        for (int i = 0, y = 30, x = 40; i < Main.userNumber; i++) {
             JButton button = new JButton();
-            if(i == 10) {
+            if (i == 10) {
                 x = 110;
                 y = 30;
-            } else if(i == 20){
+            } else if (i == 20) {
                 x = 180;
                 y = 30;
             }
@@ -102,13 +96,13 @@ public class Sort {
             button.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent actionEvent) {
-                    if(Integer.parseInt(button.getText()) <= 30){
-                        for(int i =0 ; i< buttons.size(); i++){
+                    if (Integer.parseInt(button.getText()) <= 30) {
+                        for (int i = 0; i < buttons.size(); i++) {
                             buttons.get(i).setText(String.valueOf(random.nextInt(1001)));
                         }
                         checkForThirty();
                     } else {
-                        JOptionPane.showMessageDialog(frame,"Please select a value smaller or equal to 30");
+                        JOptionPane.showMessageDialog(frame, "Please select a value smaller or equal to 30");
                     }
                 }
             });
@@ -118,30 +112,97 @@ public class Sort {
         checkForThirty();
     }
 
-    private void quickSort(int[] array,int low, int high){
+    private void quickSort(ArrayList<JButton> buttons,int low, int high){
         if(buttons.size() == 0)
             return;
-        if(low == high)
+        if(low == high) {
             return;
+        }
         int middle = low + (high - low) / 2;
-        int opora = array[middle];
+        int opora = Integer.parseInt(buttons.get(middle).getText());
         int i = low, j = high;
+        buttons.get(middle).setBackground(Color.GRAY);
+
         while (i <= j){
-            while (array[i]< opora)
+            buttons.get(i).setForeground(Color.ORANGE);
+
+            while (Integer.parseInt(buttons.get(i).getText())< opora) {
+                buttons.get(i).setForeground(Color.WHITE);
                 i++;
-            while (array[j]>opora)
+            }
+            buttons.get(j).setForeground(Color.ORANGE);
+            while (Integer.parseInt(buttons.get(j).getText())>opora) {
+                buttons.get(j).setForeground(Color.WHITE);
                 j--;
+            }
             if(i <= j){
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                int temp = Integer.parseInt(buttons.get(i).getText());
+                buttons.get(i).setText(buttons.get(j).getText());
+                buttons.get(j).setText(String.valueOf(temp));
+
+                buttons.get(i).setForeground(Color.WHITE);
+                buttons.get(i).setBackground(Color.BLUE);
+
+                buttons.get(j).setForeground(Color.WHITE);
+                buttons.get(j).setBackground(Color.BLUE);
                 i++;
                 j--;
             }
         }
         if(low < j)
-            quickSort(array, low, j);
+            quickSort(buttons, low, j);
         if(high > i)
-            quickSort(array, i, high);
+            quickSort(buttons, i, high);
+        for (int k = 0; k < buttons.size(); k++) {
+            buttons.get(k).setForeground(Color.WHITE);
+            buttons.get(k).setBackground(Color.BLUE);
+        }
+    }
+
+    private void quickSortRe(ArrayList<JButton> buttons,int low, int high){
+        if(buttons.size() == 0)
+            return;
+        if(low == high) {
+            return;
+        }
+        int middle = low + (high - low) / 2;
+        int opora = Integer.parseInt(buttons.get(middle).getText());
+        int i = low, j = high;
+        buttons.get(middle).setBackground(Color.GRAY);
+
+        while (i <= j){
+            buttons.get(i).setForeground(Color.ORANGE);
+
+            while (Integer.parseInt(buttons.get(i).getText()) > opora) {
+                buttons.get(i).setForeground(Color.WHITE);
+                i++;
+            }
+            buttons.get(j).setForeground(Color.ORANGE);
+            while (Integer.parseInt(buttons.get(j).getText())<opora) {
+                buttons.get(j).setForeground(Color.WHITE);
+                j--;
+            }
+            if(i <= j){
+                int temp = Integer.parseInt(buttons.get(j).getText());
+                buttons.get(j).setText(buttons.get(i).getText());
+                buttons.get(i).setText(String.valueOf(temp));
+
+                buttons.get(i).setForeground(Color.WHITE);
+                buttons.get(i).setBackground(Color.BLUE);
+
+                buttons.get(j).setForeground(Color.WHITE);
+                buttons.get(j).setBackground(Color.BLUE);
+                i++;
+                j--;
+            }
+        }
+        if(low < j)
+            quickSortRe(buttons, low, j);
+        if(high > i)
+            quickSortRe(buttons, i, high);
+        for (int k = 0; k < buttons.size(); k++) {
+            buttons.get(k).setForeground(Color.WHITE);
+            buttons.get(k).setBackground(Color.BLUE);
+        }
     }
 }
